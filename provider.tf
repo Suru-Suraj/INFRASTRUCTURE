3f4c56e2b946f6ec313b538276825827bc63bd38
Suru-Suraj <118275588+Suru-Suraj@users.noreply.github.com>
2023-10-04 17:58:32 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 4.0"
-      region = "us-east-1"
-    }
-    local = {
-      source = "hashicorp/local"
-      version = "~> 3.0"
-    }
-    tls = {
-      source = "hashicorp/tls"
-      version = "~> 3.0"
-    }
-  }
+provider "aws" {
+  region = "us-east-1"
 }
